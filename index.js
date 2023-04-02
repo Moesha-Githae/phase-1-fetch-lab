ea169ade3d7b6a2ed01c597ffcb915340184e888
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-04-02 14:59:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(json => {
+    renderBooks(json)
+    getTotalPages(json)
+    console.log(json[4])
+  })
 }
 
 function renderBooks(books) {
